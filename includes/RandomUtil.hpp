89cedb317f3db46245d42ee0efdfcc02f709df93
Víctor Colombo <victorcora98@gmail.com>
2020-08-14 13:11:31 -0300

    Add message drops

--- includes/RandomUtil.hpp
+++ includes/RandomUtil.hpp
@@ -8,4 +8,11 @@ class RandomUtil {
         std::uniform_int_distribution<int> dist(min, max);
         return dist(mt);
     }
+
+    static float randomFloat(float min, float max) {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<float> dist(min, max);
+        return dist(mt);
+    }
 };
--- src/Network.cpp
+++ src/Network.cpp
@@ -1,8 +1,12 @@
 #include "Network.hpp"
 
-#include "Host.hpp"
+#include <iostream>
+
 #include "Address.hpp"
+#include "Config.hpp"
+#include "Host.hpp"
 #include "Message.hpp"
+#include "RandomUtil.hpp"
 
 void Network::connect(Address addr, Host* host) {
     this->connectedHosts.insert({addr, host});
@@ -14,7 +18,11 @@ void Network::dispatchMessages() {
     while (!this->messages.empty()) {
         Message msg = this->messages.front();
 
-        this->connectedHosts[msg.to]->receiveMessage(msg);
+        if (RandomUtil::randomFloat(0.0, 1.0) > Config::MSG_DROP_CHANCE) {
+            this->connectedHosts[msg.to]->receiveMessage(msg);
+        } else {
+            std::cout << "MSG dropped\n";
+        }
 
         this->messages.pop();
     }
